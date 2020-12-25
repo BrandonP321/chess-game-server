5fe9c83dcd02f231e28037ffd59fa1da66acbaf4
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-24 22:24:13 -0800

    moved all socket listeners to /game namespace

--- server.js
+++ server.js
@@ -82,6 +82,61 @@ io.on('connection', socket => {
 io.of('/game').on('connection', socket => {
     console.log('user connected to game socket name space')
 
+
+    
+    socket.on('createNewRoom', () => {
+        // function to check if new room name is unique
+        function roomNameCheck(name) {
+            // for each existing room, check if name is equal to our new name
+            for (let i = 0; i < rooms.length; i++) {
+                const room = rooms[i]
+                // if room name is equal to our new name, return false
+                if (room.name === name) {
+                    return false
+                }
+            }
+            // if nothing is returned yet, return true
+            return true
+        }
+
+        let roomName;
+        // keep creating a new room name until a name not already being used is made
+        do {
+            roomName = generateRoomName()
+        } while (!roomNameCheck(roomName))
+
+        // create new object for the room
+        const roomObj = {
+            name: roomName,
+            whitePlayer: '',
+            blackPlayer: '',
+            teamUp: 'none',
+            watchers: []
+        }
+        console.log(roomObj)
+
+        rooms.push(roomObj)
+        console.log(rooms)
+
+        // send new roomName back to client
+        socket.emit('newRoomCreated', roomName)
+    })
+
+    // when user wants to join an existing room
+    socket.on('joinExistingRoom', (roomId) => {
+        // check for a room with the same id
+        for (let i = 0; i < rooms.length; i++) {
+            let room = rooms[i]
+            if (room.name === roomId) {
+                // if id's match, emit back the room id and break
+                socket.emit('allowRoomJoin', roomId)
+                break;
+            }
+        }
+    })
+
+
+
     let roomName;
 
     // when user joins a game, add them to a room
